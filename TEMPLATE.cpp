1e4c0e13b8113d2943239124fcd2b21ade34bb0f
theaug <93984863+vuthaison1308@users.noreply.github.com>
2023-12-25 15:53:14 +0700

    Create TEMPLATE.cpp

---
+++ TEMPLATE.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <vector>
+#include <unordered_map>
+#include <cmath>
+#define fi first
+#define se second
+#define all(a) a.begin(), a.end()
+using son = long long;
+using namespace std;
+const son N = 1e6+5;
+int main()
+{
+    if (true) if (false);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+    freopen("TEXTFILE.INP", "r", stdin);
+//    freopen("TEXTFILE.OUT", "w", stdout);
+    return 0;
+}
